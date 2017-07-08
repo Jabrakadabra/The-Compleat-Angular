6a6b318cda2f4d33be2104569e71adb0576f896f
C. Jordan Ball III <cjordanball@gmail.com>
2017-07-08 11:51:47 -0400

    add section on observables

---
+++ apps/09Observables/src/app/home/home.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observer } from 'rxjs/Observer';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/Rx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+  myNumbersSub: Subscription;
+  myObservableSub: Subscription;
+
+  constructor() { }
+
+  ngOnInit() {
+    const myNumbers = Observable.interval(100);
+    this.myNumbersSub = myNumbers.subscribe(
+      (num: number) => {
+        console.log(num);
+      }
+    );
+    const myObservable = Observable.create((observer: Observer<string>) => {
+      setTimeout(() => {
+        observer.next('first package');
+      }, 2000);
+      setTimeout(() => {
+        observer.next('second package');
+      }, 4000);
+      setTimeout(() => {
+        observer.error('this does not work');
+      }, 5000);
+      setTimeout(() => {
+        observer.complete();
+      }, 8000);
+      setTimeout(() => {
+        observer.next('third package');
+      }, 10000);
+    });
+    this.myObservableSub = myObservable.subscribe(
+      (data: string) => {
+        console.log(data);
+      },
+      (err: string) => {
+        console.log(err);
+      },
+      () => {
+        console.log('All aboard!');
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.myObservableSub.unsubscribe();
+    this.myNumbersSub.unsubscribe();
+  }
+
+}
