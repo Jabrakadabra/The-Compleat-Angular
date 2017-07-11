ea1991f81b1009b9acb273fc0783aa0bbc3d50be
C. Jordan Ball III <cjordanball@gmail.com>
2017-07-11 10:04:38 -0400

    adding add-to-list functionality

--- apps/recipeBook/src/app/services/recipe.service.ts
+++ apps/recipeBook/src/app/services/recipe.service.ts
@@ -1,15 +1,45 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable  } from '@angular/core';
 import { Recipe } from '../shared/models/recipe.model';
+import { Ingredient } from '../shared/models/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
+@Injectable()
 export class RecipeService {
 	recipeSelected = new EventEmitter<Recipe>();
 
 	private recipes: Recipe[] = [
-		new Recipe('A Test Recipe', 'This is simply a test', 'https://i1.wp.com/dippinflavors.com/wp-content/uploads/2015/04/Recipe.jpg?resize=300%2C300&ssl=1'),
-		new Recipe('Schnitzel', 'This is a lovely schnitzel', 'https://i1.wp.com/dippinflavors.com/wp-content/uploads/2015/04/Recipe.jpg?resize=300%2C300&ssl=1')
+		new Recipe(
+			'Tasty Schnitzel',
+			'A super-tasty Schnitzel - just awesome!',
+			'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+			[
+				new Ingredient('Meat', 1),
+				new Ingredient('French Fries', 20)
+			]
+		),
+		new Recipe(
+			'Big Fat Burger',
+			'What else do I need to say?',
+			'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+			[
+				new Ingredient('Bun', 1),
+				new Ingredient('Beef Patty', 2),
+				new Ingredient('Tomato Slice', 1),
+				new Ingredient('Pickle Chips', 4)
+			]
+		)
 	];
+
+	constructor(
+		private shoppingService: ShoppingListService
+	) {}
 
 	getRecipes() {
 		return this.recipes.slice();
+	}
+
+	addIngredientsToShoppingList(ingredients: Array<Ingredient>) {
+		console.log('done', ingredients);
+		this.shoppingService.addIngredients(ingredients);
 	}
 }
