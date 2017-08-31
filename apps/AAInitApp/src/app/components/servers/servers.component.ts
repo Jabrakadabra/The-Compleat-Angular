657e8d402d40447e4ea0d539272b923c0d948b9c
C. Jordan Ball III <cjordanball@gmail.com>
2017-08-31 09:13:57 -0400

    misc

--- apps/AAInitApp/src/app/components/servers/servers.component.ts
+++ apps/AAInitApp/src/app/components/servers/servers.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+	selector: 'app-servers',
+	templateUrl: './servers.component.html',
+	styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+	value: string;
+	allowNewServer = false;
+	serverCreationStatus = 'No server was created!';
 
-  constructor() { }
+	onCreateServer(toast) {
+		console.log('test0: ', toast);
+		this.serverCreationStatus = 'A server has been created!';
+	}
 
-  ngOnInit() {
-  }
+	constructor() {
+		setTimeout(() => {
+			this.allowNewServer = true;
+		}, 2000);
+	}
 
+	ngOnInit() {
+		this.value = 'Jordan';
+	}
 }
